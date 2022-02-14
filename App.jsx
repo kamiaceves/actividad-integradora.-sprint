d2b16ffeb7f4ab88faf3558346a86095c30f28d3
Kamila Aceves <54807631+kamiaceves@users.noreply.github.com>
2022-02-14 14:13:25 -0600

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { TodoList } from './components/TodoList.jsx';
+
+export function App() {
+    const [todos, setTodos] = useState([
+        {id: 1, task: 'Tarea 1', completed: false },
+        ]);
+        return <TodoList todos={todos} />;
+}
